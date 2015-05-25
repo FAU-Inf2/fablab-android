2819781f126a725eec1851c8e39742023ad1c271
Stefan Herpich <Stefan.Herpich@t-online.de>
2015-05-25 22:20:15 +0200

    fixed cart activity title representation

--- app/src/main/java/de/fau/cs/mad/fablab/android/cart/CartActivity.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/cart/CartActivity.java
@@ -2,6 +2,7 @@ package de.fau.cs.mad.fablab.android.cart;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import java.util.List;
 
@@ -21,6 +22,13 @@ public class CartActivity extends BaseActivity {
         // Setting up Basket without SlidingUpPanel
         initCartPanel(false);
         initFabButton();
+        LinearLayout drag = (LinearLayout) findViewById(R.id.dragPart);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                (int) getResources().getDimension(R.dimen.slidinguppanel_panel_height_opened) );
+
+        layoutParams.setMargins((int) getResources().getDimension(R.dimen.slidinguppanel_drag_bg_stroke_margin), 0, (int) getResources().getDimension(R.dimen.slidinguppanel_drag_bg_stroke_margin), 0);
+        drag.setLayoutParams(layoutParams);
     }
 
     @Override
