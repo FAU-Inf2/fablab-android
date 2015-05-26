be450bedd52d12ac3fa2418b2a5a80837caccb42
johannes_rechner_home <jopf88@googlemail.com>
2015-05-26 22:34:56 +0200

    - Added PushBroatCastReceiver and implemented - Added PushIntentService and implemented - Added permission for pushservice - Receiver to Androidmanifest

---
+++ app/src/main/java/de/fau/cs/mad/fablab/android/pushservice/PushBroatCastReceiver.java
@@ -0,0 +1,20 @@
+package de.fau.cs.mad.fablab.android.pushservice;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+
+public class PushBroatCastReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                PushIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
