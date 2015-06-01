bd3a4f38e9f6ad550469483e7a7927bcfd70bd6e
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-06-01 20:58:40 +0200

    adapted DatabaseHelper to changed Cart class

--- app/src/main/java/de/fau/cs/mad/fablab/android/db/DatabaseHelper.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/db/DatabaseHelper.java
@@ -18,7 +18,7 @@ import de.fau.cs.mad.fablab.rest.core.CartEntry;
 
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     private static final String DATABASE_NAME = "fablab.db";
-    private static final int DATABASE_VERSION = 11;
+    private static final int DATABASE_VERSION = 13;
     private static final String TAG = DatabaseHelper.class.getSimpleName();
 
     private static DatabaseHelper instance;
