02839b94c970827891ecf01a045815a4e12dc39b
Julian Lorz <toiano@users.noreply.github.com>
2015-06-03 12:37:52 +0200

    Initialize CartSingleton only once

--- app/src/main/java/de/fau/cs/mad/fablab/android/ui/NewsActivity.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/ui/NewsActivity.java
@@ -101,8 +101,10 @@ public class NewsActivity extends RoboActionBarActivity {
         appbarDrawer = AppbarDrawerInclude.getInstance(this);
         appbarDrawer.create();
 
-        // init db and cart - always do this on app start
-        CartSingleton.MYCART.init(getApplication());
+        if (savedInstanceState == null) {
+            // init db and cart - always do this on app start
+            CartSingleton.MYCART.init(getApplication());
+        }
 
         // init cart panel
         CartSingleton.MYCART.setSlidingUpPanel(this, findViewById(android.R.id.content), true);
