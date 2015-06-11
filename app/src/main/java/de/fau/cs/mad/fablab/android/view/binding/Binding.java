bb98d1296068ac3bede1d9001d73f2048bd7bdb1
Sebastian Haubner <sebastian.haubner@fau.de>
2015-06-11 15:28:23 +0200

    Revert "started refactoring to mvvm"
    This reverts commit 880e8c4538a24fa7f8c89147ae347cc3dfa9adb5.

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/binding/Binding.java
+++
@@ -1,4 +0,0 @@
-package de.fau.cs.mad.fablab.android.view.binding;
-
-public class Binding {
-}
--- app/src/main/java/de/fau/cs/mad/fablab/android/viewmodel/CommandListener.java
+++
@@ -1,8 +0,0 @@
-package de.fau.cs.mad.fablab.android.viewmodel;
-
-public interface CommandListener {
-
-    void onIsAvailableChanged(boolean newValue);
-
-    void onIsExecutableChanged(boolean newValue);
-}
