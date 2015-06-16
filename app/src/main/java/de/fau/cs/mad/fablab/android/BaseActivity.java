447bfaa7ed109f0aabeac0a2a162b25e5a81ab7e
Daniel Rosenmueller <info@roda-vision.de>
2015-06-16 17:37:05 +0200

    Adjusted BaseActivity to work with new made NavigationDrawer MVVM

--- app/src/main/java/de/fau/cs/mad/fablab/android/BaseActivity.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/BaseActivity.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import de.fau.cs.mad.fablab.android.cart.CartSingleton;
 import de.fau.cs.mad.fablab.android.eventbus.DoorEvent;
-import de.fau.cs.mad.fablab.android.navdrawer.AppbarDrawerInclude;
 import de.greenrobot.event.EventBus;
 
 public abstract class BaseActivity extends ActionBarActivity {
@@ -16,7 +14,7 @@ public abstract class BaseActivity extends ActionBarActivity {
     private static final int DISPLAY_CART_SLIDING_UP = 1;
     private static final int DISPLAY_CART_FULL = 2;
     // Navigation Drawer
-    public AppbarDrawerInclude appbarDrawer;
+
     private EventBus eventBus = EventBus.getDefault();
     private int cartDisplayOptions = DISPLAY_CART_NONE;
 
@@ -29,8 +27,7 @@ public abstract class BaseActivity extends ActionBarActivity {
     final public void onCreate(Bundle savedInstanceState) {
         baseSetContentView();
 
-        appbarDrawer = AppbarDrawerInclude.getInstance(this);
-        appbarDrawer.create();
+
 
         eventBus.register(this);
 
@@ -44,7 +41,6 @@ public abstract class BaseActivity extends ActionBarActivity {
     }
 
     public void onEvent(DoorEvent event) {
-        appbarDrawer.updateOpenStateEvent(event);
     }
 
     public void initFabButton() {
@@ -65,8 +61,7 @@ public abstract class BaseActivity extends ActionBarActivity {
 
     @Override
     final public boolean onCreateOptionsMenu(Menu menu) {
-        appbarDrawer.createMenu(menu);
-        appbarDrawer.startTimer();
+
         baseOnCreateOptionsMenu(menu);
         return super.onCreateOptionsMenu(menu);
     }
@@ -94,23 +89,15 @@ public abstract class BaseActivity extends ActionBarActivity {
     }
 
     protected void baseOnPause() {
-        appbarDrawer.stopTimer();
     }
 
     protected void baseOnResume() {
-        appbarDrawer.startTimer();
     }
 
     @Override
     final public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_opened) {
-            appbarDrawer.updateOpenState(item);
-            Toast appbar_opened_toast = Toast.makeText(this, appbarDrawer.openedMessage, Toast.LENGTH_SHORT);
-            appbar_opened_toast.show();
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
