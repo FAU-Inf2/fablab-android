f95b936917e90d7cb3687cc83622c801e686b3fb
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-06-19 17:29:56 +0200

    incorporated QRCodeScanner into checkout process

--- app/src/main/java/de/fau/cs/mad/fablab/android/cart/CheckoutActivity2.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/cart/CheckoutActivity2.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.Toast;
 
+import com.google.zxing.Result;
 import com.j256.ormlite.dao.ForeignCollection;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
+import de.fau.cs.mad.fablab.android.R;
+import de.fau.cs.mad.fablab.android.barcodescanner.ScannerFragment;
 import de.fau.cs.mad.fablab.android.db.DatabaseHelper;
 import de.fau.cs.mad.fablab.rest.CartApiClient;
 import de.fau.cs.mad.fablab.rest.core.Cart;
@@ -18,23 +20,46 @@ import de.fau.cs.mad.fablab.rest.core.CartEntry;
 import de.fau.cs.mad.fablab.rest.core.CartEntryServer;
 import de.fau.cs.mad.fablab.rest.core.CartServer;
 import de.fau.cs.mad.fablab.rest.core.CartStatusEnum;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class CheckoutActivity2 extends ActionBarActivity {
+public class CheckoutActivity2 extends ActionBarActivity implements ZXingScannerView.ResultHandler{
 
     private String cartID;
     private RuntimeExceptionDao<Cart, String> cartDao;
+    private ScannerFragment mScannerFragment;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.fragment_container);
+
+        if (savedInstanceState == null) {
+            mScannerFragment = ScannerFragment.newInstance(getResources().getString(
+                    R.string.title_scan_qr_code));
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
+                    mScannerFragment, "scanner").commit();
+        } else {
+            mScannerFragment = (ScannerFragment) getSupportFragmentManager().findFragmentByTag(
+                    "scanner");
+        }
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        String qrCodeText = result.getText();
+
+        getSupportFragmentManager().beginTransaction().remove(mScannerFragment).commit();
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         //create random ID
-        Random random = new Random();
-        long cartIDLong = random.nextLong();
-        cartID = Long.toString(cartIDLong);
+        //Random random = new Random();
+        //long cartIDLong = random.nextLong();
+        //cartID = Long.toString(cartIDLong);
+        cartID = qrCodeText;
 
         //get cart and cartEntries from CartSingleton
         Cart cart = CartSingleton.MYCART.getCart();
