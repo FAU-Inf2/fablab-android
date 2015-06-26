2d09c68e248c35e80d597ddbaf12179d21aae674
Stefan Herpich <Stefan.Herpich@t-online.de>
2015-06-26 20:00:23 +0200

    totally reworked undo functionality in cart; fixed scroll bug in cart; fixed price preview after cart change; fixed representation of long unit names

--- app/src/main/java/de/fau/cs/mad/fablab/android/cart/RecyclerViewAdapter.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/cart/RecyclerViewAdapter.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -33,25 +32,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         private TextView cart_product_price;
         private Spinner cart_product_quantity_spinner;
         private TextView cart_product_quantity;
-        private Button cart_product_undo;
-        private TextView cart_product_removed;
-        private LinearLayout ll;
-        private ImageView cart_product_undo_img;
-
-
 
         ProductViewHolder(View itemView) {
             super(itemView);
-
-            this.ll = (LinearLayout) itemView.findViewById(R.id.cart_entry_undo);
-            this.cart_product_removed = (TextView) itemView.findViewById(R.id.cart_product_removed);
-            this.cart_product_undo = (Button) itemView.findViewById(R.id.cart_product_undo);
             this.cart_product_quantity = (TextView) itemView.findViewById(R.id.cart_product_quantity);
             this.cart_product_quantity_spinner = (Spinner) itemView.findViewById(R.id.cart_product_quantity_spinner);
             this.cart_product_name = (TextView) itemView.findViewById(R.id.cart_product_name);
             this.cart_product_photo = (ImageView) itemView.findViewById(R.id.cart_product_photo);
             this.cart_product_price = (TextView) itemView.findViewById(R.id.cart_product_price);
-            this.cart_product_undo_img = (ImageView) itemView.findViewById(R.id.cart_product_undo_img);
         }
     }
 
@@ -79,14 +67,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(final ProductViewHolder productViewHolder, final int i) {
-        if(CartSingleton.MYCART.getIsProductRemoved().get(i) == true){
-            productViewHolder.ll.setVisibility(View.VISIBLE);
-        }else{
-            productViewHolder.ll.setVisibility(View.GONE);
-        }
 
         String[] name = products.get(i).getProduct().getName().split(" ");
 
+        // make the first word of the product name bold
         String product_name = "";
         for(int k=0;k<name.length;k++){
             if(k == 0){
@@ -100,15 +84,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             }
         }
 
+        // set product name
         productViewHolder.cart_product_name.setText(Html.fromHtml(product_name));
+
+        // set product price bold in the following way: X,XX €
         String formated_price = String.format("%.2f", products.get(i).getProduct().getPrice());
         productViewHolder.cart_product_price.setText(formated_price +
                 Html.fromHtml(productViewHolder.itemView.getResources().getString(R.string.non_breaking_space)) +
                 Html.fromHtml(productViewHolder.itemView.getResources().getString(R.string.currency)));
-        productViewHolder.cart_product_photo.setImageResource(R.drawable.no_image_avl);
-        productViewHolder.cart_product_removed.setText(productViewHolder.itemView.getResources().getString(R.string.cart_product_removed));
-        productViewHolder.cart_product_undo.setText(productViewHolder.itemView.getResources().getString(R.string.cart_product_undo));
 
+        // set img
+        productViewHolder.cart_product_photo.setImageResource(R.drawable.no_image_avl);
 
         // check the availability of attribute "unit" from product
         String italic_unit = productViewHolder.itemView.getResources().getString(R.string.cart_product_quantity) +
@@ -123,8 +109,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         // Set unit text/quantity title
         productViewHolder.cart_product_quantity.setText(Html.fromHtml(italic_unit));
 
-        List<String> list = new ArrayList<>();
         // TODO: adapt this later to the available amount of the product
+        // set spinner range to change quantity
+        List<String> list = new ArrayList<>();
         int qty = (int) products.get(i).getAmount();
         int start = 1;
         if(qty-500 > 1)
@@ -136,9 +123,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         ArrayAdapter<String> aa=new ArrayAdapter<>(context,R.layout.cart_entry_quantity_spinner_item, list);
         aa.setDropDownViewResource(R.layout.cart_entry_quantity_spinner_dropdown);
-
         productViewHolder.cart_product_quantity_spinner.setAdapter(aa);
-
         productViewHolder.cart_product_quantity_spinner.setSelection(qty - 1);
 
         // Set up listener for quantity change
@@ -149,7 +134,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                     double amount_new = Double.parseDouble(((String) parent.getSelectedItem()).replaceAll("\\s+", ""));
                     products.get(i).setAmount(amount_new);
                     CartSingleton.MYCART.updateVisibility();
-                    CartSingleton.MYCART.updateProducts(i);
+                    CartSingleton.MYCART.updateProducts(products.get(i));
                     CartSingleton.MYCART.refresh();
                 }
             }
@@ -160,24 +145,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             }
         });
 
-        // Set up undo button listener
-        productViewHolder.cart_product_undo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CartSingleton.MYCART.addRemovedProduct(i);
-                CartSingleton.MYCART.getIsProductRemoved().set(i, false);
-                productViewHolder.ll.setVisibility(View.GONE);
-                productViewHolder.ll.setClickable(false);
-            }
-        });
-
-        // Set up undo img click listener (same behavior as undo button)
-        productViewHolder.cart_product_undo_img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                productViewHolder.cart_product_undo.performClick();
-            }
-        });
 
         // Set up click listener
         CardView cv = (CardView) productViewHolder.itemView;
@@ -205,8 +172,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                     int y = (int) e.getRawY();
 
                     // show / hide full product title
-                    if(!inViewInBounds(productViewHolder.cart_product_quantity_spinner, x, y) &&
-                            productViewHolder.ll.getVisibility() == View.GONE){
+                    if(!inViewInBounds(productViewHolder.cart_product_quantity_spinner, x, y)){
                         if(productViewHolder.cart_product_name.getLineCount() == 1){
                             productViewHolder.cart_product_name.setSingleLine(false);
                         }else{
@@ -232,7 +198,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 }
             });
 
-            // Check whether pos(x,y) is in View
+            // Check whether pos(x,y) is in the given View
             public boolean inViewInBounds(View view, int x, int y){
                 Rect outRect = new Rect();
                 int[] location = new int[2];
