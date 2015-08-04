048e68af680cc90d35dc11011109163647f1bd4a
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-08-04 08:05:22 +0200

    added functionality to AlertFagment and corresponding ViewModel

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/alert/AlertDialogFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/alert/AlertDialogFragment.java
@@ -1,20 +1,31 @@
 package de.fau.cs.mad.fablab.android.view.fragments.alert;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import butterknife.InjectView;
 import de.fau.cs.mad.fablab.android.R;
+import de.fau.cs.mad.fablab.android.util.UiUtils;
+import de.fau.cs.mad.fablab.android.view.common.binding.ViewCommandBinding;
 import de.fau.cs.mad.fablab.android.view.common.fragments.BaseDialogFragment;
+import de.fau.cs.mad.fablab.rest.core.FabTool;
 
-public class AlertDialogFragment extends BaseDialogFragment {
+public class AlertDialogFragment extends BaseDialogFragment
+    implements AlertDialogFragmentViewModel.Listener
+{
 
     @InjectView(R.id.alert_dialog_tool_spinner)
     Spinner mToolSpinner;
@@ -30,12 +41,54 @@ public class AlertDialogFragment extends BaseDialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         return inflater.inflate(R.layout.alert_dialog, container, false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        mViewModel.setListener(this);
+        mEditText.setText("");
+
+        //set spinner items
+        List<String> mToolNames = new ArrayList<>();
+        List<FabTool> mTools = mViewModel.getTools();
+        for(FabTool f : mTools)
+        {
+            mToolNames.add(f.getTitle());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                R.layout.spinner_item, mToolNames);
+        mToolSpinner.setAdapter(adapter);
+
+        new ViewCommandBinding().bind(mOKButton, mViewModel.getOKCommand());
+        new ViewCommandBinding().bind(mCancelButton, mViewModel.getCancelCommand());
+    }
+
+    @Override
+    public void onOK() {
+        Intent sendIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", mViewModel.getMailAddress(), null));
+        String subject = getActivity().getString(R.string.alert_messaging_subject);
+        String body = "tool name: \n" + mToolSpinner.getSelectedItem() + "\n";
+        body += "problem: \n" + mEditText.getText() + "\n";
+
+        sendIntent.putExtra(Intent.EXTRA_TEXT, body);
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+
+        getActivity().startActivity(Intent.createChooser(sendIntent, getActivity().getString(
+                R.string.alert_messaging_chooser_title)));
+
+        dismiss();
+        UiUtils.hideKeyboard(getActivity());
+        getFragmentManager().popBackStack();
+    }
+
+    @Override
+    public void onCancel() {
+        dismiss();
+        UiUtils.hideKeyboard(getActivity());
+        getFragmentManager().popBackStack();
     }
 }
--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/alert/AlertDialogFragmentViewModel.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/alert/AlertDialogFragmentViewModel.java
@@ -1,15 +1,76 @@
 package de.fau.cs.mad.fablab.android.view.fragments.alert;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import de.fau.cs.mad.fablab.android.model.DrupalModel;
+import de.fau.cs.mad.fablab.android.model.FablabMailModel;
+import de.fau.cs.mad.fablab.android.viewmodel.common.commands.Command;
+import de.fau.cs.mad.fablab.rest.core.FabTool;
 
 public class AlertDialogFragmentViewModel {
 
     private DrupalModel mDrupalModel;
+    private FablabMailModel mFablablMailModel;
+    private Listener mListener;
+
+    private Command<Void> mOKCommand = new Command<Void>()
+    {
+        @Override
+        public void execute(Void parameter) {
+            if (mListener != null) {
+                mListener.onOK();
+            }
+        }
+    };
+
+    private Command<Void> mCancelCommand = new Command<Void>()
+    {
+        @Override
+        public void execute(Void parameter) {
+            if (mListener != null) {
+                mListener.onCancel();
+            }
+        }
+    };
 
     @Inject
-    public AlertDialogFragmentViewModel(DrupalModel drupalModel) {
+    public AlertDialogFragmentViewModel(DrupalModel drupalModel, FablabMailModel fablabMailModel)
+    {
         mDrupalModel = drupalModel;
+        mFablablMailModel = fablabMailModel;
+    }
+
+    public void setListener(Listener listener)
+    {
+        mListener = listener;
+    }
+
+    public Command<Void> getOKCommand()
+    {
+        return mOKCommand;
+    }
+
+    public Command<Void> getCancelCommand()
+    {
+        return mCancelCommand;
+    }
+
+    public List<FabTool> getTools()
+    {
+        return mDrupalModel.getFabTools();
+    }
+
+    public String getMailAddress()
+    {
+        return mFablablMailModel.getMailAddress();
+    }
+
+    public interface Listener
+    {
+        void onOK();
+
+        void onCancel();
     }
 }
