7696d7cc938a5b1f863694fadb6c16e43478f0f2
Daniel Rosenmueller <info@roda-vision.de>
2015-08-05 20:01:04 +0200

    Settings fragment now closed navigation drawer

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/settings/SettingsFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/settings/SettingsFragment.java
@@ -5,18 +5,28 @@ import android.os.Bundle;
 import com.github.machinarius.preferencefragment.PreferenceFragment;
 
 import de.fau.cs.mad.fablab.android.R;
+import de.fau.cs.mad.fablab.android.view.activities.MainActivity;
 
 /**
  * Simple Settings/Preferences Fragment to provide some user configurable options.
  */
 public class SettingsFragment extends PreferenceFragment {
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.settings);
-        }
-
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.settings);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity activity = (MainActivity) getActivity();
+        activity.enableNavigationDrawer(true);
+        activity.setNavigationDrawerSelection(R.id.drawer_item_settings);
+        activity.showFloatingActionButton(false);
+        activity.showCartSlidingUpPanel(false);
+    }
+}
