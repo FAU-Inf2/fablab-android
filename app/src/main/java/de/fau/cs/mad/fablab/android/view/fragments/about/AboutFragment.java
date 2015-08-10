0a23d04b58f10765c83e3809ef82b8725d338959
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-08-10 20:15:04 +0200

    made about screen nicer (toggle)

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/about/AboutFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/about/AboutFragment.java
@@ -14,26 +14,36 @@ import javax.inject.Inject;
 
 import butterknife.InjectView;
 import de.fau.cs.mad.fablab.android.R;
+import de.fau.cs.mad.fablab.android.view.common.binding.ViewCommandBinding;
 import de.fau.cs.mad.fablab.android.view.common.fragments.BaseFragment;
 
-public class AboutFragment extends BaseFragment {
+public class AboutFragment extends BaseFragment implements AboutFragmentViewModel.Listener {
     @InjectView(R.id.about_version)
     TextView version_tv;
     @InjectView(R.id.about_own_license)
     TextView own_license_tv;
+    @InjectView(R.id.about)
+    TextView about_tv;
     @InjectView(R.id.about_content)
     TextView content_tv;
+    @InjectView(R.id.about_open_source_licenses)
+    TextView about_open_source_licenses_tv;
     @InjectView(R.id.about_used_libraries)
     TextView used_libraries_tv;
 
     @Inject
-    public AboutFragment() {
+    AboutFragmentViewModel mViewModel;
+
+    @Inject
+    public AboutFragment(){
 
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        mViewModel.setListener(this);
 
         String version;
         try {
@@ -43,13 +53,25 @@ public class AboutFragment extends BaseFragment {
         } catch (PackageManager.NameNotFoundException e) {
             throw new RuntimeException(e);
         }
+
+
         version_tv.setText(version);
+        new ViewCommandBinding().bind(version_tv, mViewModel.getToggleVersionCommand());
+
         own_license_tv.setText(Html.fromHtml(getString(R.string.about_license)));
+        own_license_tv.setVisibility(View.GONE);
+
+        new ViewCommandBinding().bind(about_tv, mViewModel.getToggleAboutCommand());
+
         content_tv.setText(Html.fromHtml(getString(R.string.about_content)));
         content_tv.setMovementMethod(LinkMovementMethod.getInstance());
+        content_tv.setVisibility(View.GONE);
+
+        new ViewCommandBinding().bind(about_open_source_licenses_tv, mViewModel.getToggleOpenSourceLicensesCommand());
 
         used_libraries_tv.setText(Html.fromHtml(getString(R.string.about_used_libraries)));
         used_libraries_tv.setMovementMethod(LinkMovementMethod.getInstance());
+        used_libraries_tv.setVisibility(View.GONE);
     }
 
     @Override
@@ -62,5 +84,24 @@ public class AboutFragment extends BaseFragment {
     public void onResume() {
         super.onResume();
         setDisplayOptions(R.id.drawer_item_about, false, false);
+    }
+
+    private void toogle(TextView tv){
+        tv.setVisibility(tv.isShown() ? View.GONE : View.VISIBLE);
+    }
+
+    @Override
+    public void toggleVersion() {
+        toogle(own_license_tv);
+    }
+
+    @Override
+    public void toggleAbout() {
+        toogle(content_tv);
+    }
+
+    @Override
+    public void toggleOpenSourceLicenses() {
+        toogle(used_libraries_tv);
     }
 }
