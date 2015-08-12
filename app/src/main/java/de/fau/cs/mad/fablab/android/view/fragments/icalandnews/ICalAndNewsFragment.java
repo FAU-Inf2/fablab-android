5a933df93a09e38df3b8bed81b8f9ff337c21721
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-08-12 14:17:54 +0200

    removed cause of gap in iCal and News Fragment

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/icalandnews/ICalAndNewsFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/icalandnews/ICalAndNewsFragment.java
@@ -5,22 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.sothree.slidinguppanel.SlidingUpPanelLayout;
-
 import javax.inject.Inject;
 
-import butterknife.InjectView;
 import de.fau.cs.mad.fablab.android.R;
 import de.fau.cs.mad.fablab.android.view.common.fragments.BaseFragment;
 import de.fau.cs.mad.fablab.android.view.fragments.icals.ICalFragment;
 import de.fau.cs.mad.fablab.android.view.fragments.news.NewsFragment;
 
 public class ICalAndNewsFragment extends BaseFragment {
-
-    @InjectView(R.id.sliding_layout)
-    SlidingUpPanelLayout slidingUpPanel;
-
-    private final int parallaxOffset = 180;
 
     @Inject
     public ICalAndNewsFragment() {
@@ -36,12 +28,6 @@ public class ICalAndNewsFragment extends BaseFragment {
     public void onResume() {
         super.onResume();
         setDisplayOptions(R.id.drawer_item_news, true, true);
-
-        if(slidingUpPanel.getPanelState().equals(SlidingUpPanelLayout.PanelState.EXPANDED))
-        {
-            slidingUpPanel.setParalaxOffset(0);
-        }
-
     }
 
     @Override
@@ -55,33 +41,6 @@ public class ICalAndNewsFragment extends BaseFragment {
 
         final String TAG_ICAL_FRAGMENT = "tag_ical_fragment";
         final String TAG_NEWS_FRAGMENT = "tag_news_fragment";
-
-        slidingUpPanel.setParalaxOffset(parallaxOffset);
-
-        slidingUpPanel.setPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
-            @Override
-            public void onPanelSlide(View view, float v) {
-            }
-
-            @Override
-            public void onPanelCollapsed(View view) {
-                slidingUpPanel.setParalaxOffset(parallaxOffset);
-            }
-
-            @Override
-            public void onPanelExpanded(View view) {
-            }
-
-            @Override
-            public void onPanelAnchored(View view) {
-                slidingUpPanel.setParalaxOffset(parallaxOffset);
-            }
-
-            @Override
-            public void onPanelHidden(View view) {
-                slidingUpPanel.setParalaxOffset(parallaxOffset);
-            }
-        });
 
         ICalFragment iCalFragment = (ICalFragment) getChildFragmentManager().findFragmentByTag(
                 TAG_ICAL_FRAGMENT);
