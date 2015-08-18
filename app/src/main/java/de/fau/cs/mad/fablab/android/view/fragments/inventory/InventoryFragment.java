badf02eccf4267962b5458a4951a7f3282a1cf03
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-08-18 14:34:24 +0200

    added buttons for scan and search of products for inventory with corresponding commands (no functionality yet)

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/inventory/InventoryFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/inventory/InventoryFragment.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import javax.inject.Inject;
 
+import butterknife.InjectView;
 import de.fau.cs.mad.fablab.android.R;
+import de.fau.cs.mad.fablab.android.view.common.binding.ViewCommandBinding;
 import de.fau.cs.mad.fablab.android.view.common.fragments.BaseFragment;
 
-public class InventoryFragment extends BaseFragment {
+public class InventoryFragment extends BaseFragment implements InventoryFragmentViewModel.Listener{
+
+    @InjectView(R.id.button_scan_product_inventory)
+    Button mScanButton;
+    @InjectView(R.id.button_search_product_inventory)
+    Button mSearchButton;
+
+    @Inject
+    InventoryFragmentViewModel mViewModel;
 
     @Inject
     public InventoryFragment(){
@@ -20,6 +31,11 @@ public class InventoryFragment extends BaseFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        mViewModel.setListener(this);
+
+        new ViewCommandBinding().bind(mScanButton, mViewModel.getOnScanButtonClickedCommand());
+        new ViewCommandBinding().bind(mSearchButton, mViewModel.getOnSearchButtonClickedCommand());
     }
 
     @Override
@@ -32,5 +48,15 @@ public class InventoryFragment extends BaseFragment {
     public void onResume() {
         super.onResume();
         setDisplayOptions(R.id.drawer_item_inventory, false, false);
+    }
+
+    @Override
+    public void onScanButtonClicked() {
+
+    }
+
+    @Override
+    public void onSearchButtonClicked() {
+
     }
 }
--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/inventory/InventoryFragmentViewModel.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/inventory/InventoryFragmentViewModel.java
@@ -1,4 +1,58 @@
 package de.fau.cs.mad.fablab.android.view.fragments.inventory;
 
+import javax.inject.Inject;
+
+import de.fau.cs.mad.fablab.android.viewmodel.common.commands.Command;
+
 public class InventoryFragmentViewModel {
+
+    private Listener mListener;
+
+    private Command<Void> mOnScanButtonClickedCommand = new Command<Void>()
+    {
+        @Override
+        public void execute(Void parameter) {
+            if (mListener != null) {
+                mListener.onScanButtonClicked();
+            }
+        }
+    };
+
+    private Command<Void> mOnSearchButtonClickedCommand = new Command<Void>()
+    {
+        @Override
+        public void execute(Void parameter) {
+            if (mListener != null) {
+                mListener.onSearchButtonClicked();
+            }
+        }
+    };
+
+    @Inject
+    public InventoryFragmentViewModel()
+    {
+
+    }
+
+    public void setListener(Listener listener)
+    {
+        mListener = listener;
+    }
+
+    public Command<Void> getOnScanButtonClickedCommand()
+    {
+        return mOnScanButtonClickedCommand;
+    }
+
+    public Command<Void> getOnSearchButtonClickedCommand()
+    {
+        return mOnSearchButtonClickedCommand;
+    }
+
+    public interface Listener
+    {
+        void onScanButtonClicked();
+
+        void onSearchButtonClicked();
+    }
 }
