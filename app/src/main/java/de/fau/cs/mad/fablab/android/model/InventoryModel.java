147e48233a66e24bda4dc9f3d5d86c9515c39297
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-08-23 13:22:32 +0200

    adapted InventoryModel to get whole inventory from server

--- app/src/main/java/de/fau/cs/mad/fablab/android/model/InventoryModel.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/model/InventoryModel.java
@@ -1,9 +1,12 @@
 package de.fau.cs.mad.fablab.android.model;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import de.fau.cs.mad.fablab.android.model.events.InventoryDeletedEvent;
 import de.fau.cs.mad.fablab.android.model.events.InventoryNotDeletedEvent;
+import de.fau.cs.mad.fablab.android.viewmodel.common.ObservableArrayList;
 import de.fau.cs.mad.fablab.rest.core.InventoryItem;
 import de.fau.cs.mad.fablab.rest.myapi.InventoryApi;
 import de.greenrobot.event.EventBus;
@@ -15,6 +18,7 @@ public class InventoryModel {
 
     private InventoryApi mInventoryApi;
     private EventBus mEventBus = EventBus.getDefault();
+    private ObservableArrayList<InventoryItem> mItems;
 
     private Callback<InventoryItem> mAddCallback = new Callback<InventoryItem>() {
         @Override
@@ -40,9 +44,23 @@ public class InventoryModel {
         }
     };
 
+    private Callback<List<InventoryItem>> mGetCallback = new Callback<List<InventoryItem>>() {
+        @Override
+        public void success(List<InventoryItem> items, Response response) {
+            mItems.clear();
+            mItems.addAll(items);
+        }
+
+        @Override
+        public void failure(RetrofitError error) {
+
+        }
+    };
+
     @Inject
     public InventoryModel(InventoryApi api){
         mInventoryApi = api;
+        mItems = new ObservableArrayList<>();
     }
 
     public void sendInventoryItem(InventoryItem item)
@@ -53,5 +71,15 @@ public class InventoryModel {
     public void deleteInventory()
     {
         mInventoryApi.deleteAll(mDeleteCallback);
+    }
+
+    public void getInventory()
+    {
+        mInventoryApi.getAll(mGetCallback);
+    }
+
+    public ObservableArrayList<InventoryItem> getInventoryItems()
+    {
+        return mItems;
     }
 }
