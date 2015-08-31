6f62124910bb59a954e3c743e1f4f50cd08a1dd3
michaelsandner <michaelsandy@gmx.de>
2015-08-31 22:03:03 +0200

    ignore sslerror in productmap enabled

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/productmap/ProductMapFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/productmap/ProductMapFragment.java
@@ -2,6 +2,7 @@ package de.fau.cs.mad.fablab.android.view.fragments.productmap;
 
 import android.content.pm.ActivityInfo;
 import android.net.http.SslError;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,25 +11,25 @@ import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import org.apache.http.conn.scheme.SchemeRegistry;
-
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 
-import butterknife.InjectView;
+//import butterknife.InjectView;
 import butterknife.Bind;
 import de.fau.cs.mad.fablab.android.R;
 import de.fau.cs.mad.fablab.android.view.common.fragments.BaseDialogFragment;
 
-public class ProductMapFragment extends BaseDialogFragment {
+public class ProductMapFragment extends BaseDialogFragment implements CallBackListener
+{
     public static final String KEY_LOCATION = "key_location";
-    public static final String TRUSTSTORE_KEY = "gWWB5-P5eiVL6evqC cI274Kq-bjYc-P719gaYrp-1D_zRw4pA _7KsPsRFW4ivjib5i7bW3fvnXvwLgtkGPU88ob_C_0fvXV_RN";
+    private CallBackListener callBackListener = this;
 
     @Bind(R.id.location_webView)
     WebView webview;
@@ -37,71 +38,28 @@ public class ProductMapFragment extends BaseDialogFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         //Todo: activate when server method is finished
         String productMapUrl = getString(R.string.api_url) + "/productMap/productMap.html";
         String locationString = getArguments().getString(KEY_LOCATION); //something like "fau fablab / werkstadt / ..."
 
+        locationString = locationString.replace("_/", "/");
+        locationString = locationString.replace(",_","_");
         locationString = locationString.replace("ä", "ae");
         locationString = locationString.replace("ö", "oe");
         locationString = locationString.replace("ü", "ue");
         locationString = locationString.replace("ß", "ss");
 
+        //test with local asset
+        //productMapUrl = "file:///android_asset/productMap.html?id=Werkbank";
 
         String url = "";
         if(locationString != "")
             url = productMapUrl + "?id=" + locationString;
         else url = productMapUrl;
 
-//        try
-//        {
-            //1
-//            KeyStore localTrustStore = KeyStore.getInstance("BKS");
-//            InputStream in = getResources().openRawResource(R.raw.fablab_dev_truststore);
-//            localTrustStore.load(in, TRUSTSTORE_KEY.toCharArray());
-//
-//            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-//            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
-//            tmf.init(localTrustStore);
-//
-//            SSLContext context = SSLContext.getInstance("TLS");
-//            context.init(null, tmf.getTrustManagers(), null);
-//
-//            URL newUrl = new URL(url);
-//            HttpsURLConnection urlConnection = (HttpsURLConnection)newUrl.openConnection();
-//            urlConnection.setSSLSocketFactory(context.getSocketFactory());
-//            InputStream inputStream = urlConnection.getInputStream();
-            //2
-        try
-        {
-            //load Truststore
-            KeyStore trustStore = KeyStore.getInstance("PKCS12");
-            InputStream in = getResources().openRawResource(R.raw.fablab_dev_truststore);
-            trustStore.load(in, TRUSTSTORE_KEY.toCharArray());
-
-            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            tmf.init(trustStore);
-
-            SSLContext sslCtx = SSLContext.getInstance("TLS");
-            sslCtx.init(null, tmf.getTrustManagers(), null);
-
-            URL newUrl = new URL(url);
-            HttpsURLConnection urlConnection = (HttpsURLConnection) newUrl.openConnection();
-            urlConnection.setSSLSocketFactory(sslCtx.getSocketFactory());
-
-
-           // webview.loadData(urlConnection.getContent(), "text/html; charset=UTF-8", null);
-            // have to read content and then pass to webview
-
-        } catch (KeyStoreException e)
-        {
-            e.printStackTrace();
-        }
-        catch (Exception e)
-        {
-
-        }
 
         /*3 creating a WebViewClient and override the
         shouldOverrideUrlLoading method to setup a SSLSocket and pass the
@@ -110,42 +68,56 @@ public class ProductMapFragment extends BaseDialogFragment {
         should use to setup SSL connections.*/
 
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.setWebViewClient(new WebViewClient()
-        {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url)
-            {
-                //setup sslsocket
 
-                //url="http://google.com";
-                view.loadUrl(url);
-                System.out.println("hello");
-                return true;
-            }
-        });
-        //Toast.makeText(this, "", Toast.LENGTH_SHORT);
-        webview.loadUrl(url);
-
-
-//            webview.getSettings().setJavaScriptEnabled(true);
-//            webview.loadUrl(url);
-//
-//        } catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
-
-        webview.getSettings().setJavaScriptEnabled(true);
+        final String finalUrl = url;
         webview.loadUrl(url);
         webview.setWebViewClient(new WebViewClient()
         {
+
             @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error)
+            public void onReceivedSslError (WebView view, SslErrorHandler handler, SslError error)
             {
+
                 handler.proceed();
+                // next release
+//                try
+//                {
+//                    //Default type is BKS on android
+//                    KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+//                    //our truststore containing the public certs
+//                    InputStream inputStream = getResources().openRawResource(R.raw.fablab_dev_truststore);
+//
+//                    //the password used here is just a dummy as it is needed by the keystore.load method
+//                    String trustStorePass = "dummypass4dev";
+//                    keyStore.load(inputStream, trustStorePass.toCharArray());
+//
+//                    SSLContext sslContext = SSLContext.getInstance("TLS");
+//                    TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+//                            TrustManagerFactory.getDefaultAlgorithm());
+//                    trustManagerFactory.init(keyStore);
+//                    sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+//
+//
+//
+//                    ConnectionTask connectionTask = new ConnectionTask();
+//                    connectionTask.setListener(callBackListener);
+//
+//                    connectionTask.execute(finalUrl);
+//
+////                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+////
+//
+//                }
+//                catch (android.os.NetworkOnMainThreadException e)
+//                {
+//                    handler.proceed();
+//                }
+//                catch (Exception e)
+//                {
+//                    e.printStackTrace();
+//                }
             }
         });
-
 
     }
 
@@ -165,5 +137,90 @@ public class ProductMapFragment extends BaseDialogFragment {
     public void onResume() {
         super.onResume();
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        }
+
+    @Override
+    public void callback(BufferedReader bufferedReader)
+    {
+        try
+        {
+            String htmlText = "";
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null)
+                htmlText += line;
+
+            webview.loadData(htmlText, "text/html; charset=UTF-8", null);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
+
+    // next release
+    private class ConnectionTask extends AsyncTask<String, Integer , BufferedReader>
+    {
+        private CallBackListener mListener;
+
+
+        public void setListener(CallBackListener listener)
+        {
+            mListener = listener;
+        }
+
+
+        @Override
+        protected BufferedReader doInBackground(String... urls)
+        {
+            BufferedReader bufferedReader;
+            URL myUrl;
+            HttpsURLConnection urlConnection;
+              try
+            {
+                //Default type is BKS on android
+                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                //our truststore containing the public certs
+                InputStream inputStream = getResources().openRawResource(R.raw.fablab_dev_truststore);
+
+                //the password used here is just a dummy as it is needed by the keystore.load method
+                String trustStorePass = "dummypass4dev";
+                keyStore.load(inputStream, trustStorePass.toCharArray());
+
+                SSLContext sslContext = SSLContext.getInstance("TLS");
+                TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                        TrustManagerFactory.getDefaultAlgorithm());
+                trustManagerFactory.init(keyStore);
+                sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+
+                myUrl = new URL(urls[0]);
+                urlConnection = (HttpsURLConnection) myUrl.openConnection();
+                urlConnection.setSSLSocketFactory(sslContext.getSocketFactory());
+
+
+                bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                return bufferedReader;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(BufferedReader bufferedReader) {
+            mListener.callback(bufferedReader);
+        }
+    }
+
 }
+interface CallBackListener
+{
+    public void callback(BufferedReader bufferedReader);
+}
+
