137fdc9581d7bcb289f2b2c1662fde0022e32af9
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-09-09 19:19:33 +0200

    add search field in productsearch in inventory

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/inventory/InventoryProductSearchFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/inventory/InventoryProductSearchFragment.java
@@ -1,8 +1,6 @@
 package de.fau.cs.mad.fablab.android.view.fragments.inventory;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 
 import de.fau.cs.mad.fablab.android.R;
 import de.fau.cs.mad.fablab.android.view.fragments.productsearch.ProductClickedEvent;
@@ -26,13 +24,7 @@ public class InventoryProductSearchFragment extends ProductSearchFragment {
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(false);
         mUser = (User) getArguments().getSerializable("USER");
-    }
-
-    @Override
-    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
