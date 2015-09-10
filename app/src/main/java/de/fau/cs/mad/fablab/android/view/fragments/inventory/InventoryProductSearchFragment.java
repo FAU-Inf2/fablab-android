5abd23324762ee87529338fcbcb89a3fa8b59bd5
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-09-10 13:11:38 +0200

    adapt inventory product search to new structure of product search

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/inventory/InventoryProductSearchFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/inventory/InventoryProductSearchFragment.java
@@ -10,6 +10,7 @@ import de.fau.cs.mad.fablab.rest.core.User;
 public class InventoryProductSearchFragment extends ProductSearchFragment {
 
     private User mUser;
+    private boolean mShowCartFAB;
 
     public void setUser(User user)
     {
@@ -24,13 +25,15 @@ public class InventoryProductSearchFragment extends ProductSearchFragment {
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mUser = (User) getArguments().getSerializable(getResources().getString(R.string.key_user));
+        Bundle args = getArguments();
+        mUser = (User) args.getSerializable(getResources().getString(R.string.key_user));
+        mShowCartFAB = args.getBoolean(getResources().getString(R.string.key_show_cart_fab));
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        setDisplayOptions(R.id.drawer_item_inventory, true, false, false);
+        setDisplayOptions(R.id.drawer_item_inventory, true, mShowCartFAB, mShowCartFAB);
     }
 
     @Override
