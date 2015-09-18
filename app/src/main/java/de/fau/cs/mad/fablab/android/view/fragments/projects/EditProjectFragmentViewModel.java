afd01f494b7a503ca7b7057f604bcfa6002fa911
Julia Schottenhamml <julia.schottenhamml@medtech.stud.uni-erlangen.de>
2015-09-18 20:58:59 +0200

    added basic fragment to create and edit projects (no functionality yet)

---
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/projects/EditProjectFragmentViewModel.java
@@ -0,0 +1,6 @@
+package de.fau.cs.mad.fablab.android.view.fragments.projects;
+
+import de.fau.cs.mad.fablab.android.view.common.fragments.BaseFragment;
+
+public class EditProjectFragmentViewModel extends BaseFragment {
+}
