ecf4899e7464fab944de50ff8e2a1df1ed3bfdc0
michaelsandner <michaelsandy@gmx.de>
2015-10-01 17:13:36 +0200

    change widget provider

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/productmap/ProductMapFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/productmap/ProductMapFragment.java
@@ -1,6 +1,5 @@
 package de.fau.cs.mad.fablab.android.view.fragments.productmap;
 
-import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,41 +8,15 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.Toast;
 
-<<<<<<< HEAD
 import javax.inject.Inject;
-=======
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.security.KeyStore;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
->>>>>>> change productmap
 
 import butterknife.Bind;
 import de.fau.cs.mad.fablab.android.R;
 import de.fau.cs.mad.fablab.android.view.activities.MainActivity;
 import de.fau.cs.mad.fablab.android.view.common.fragments.BaseDialogFragment;
 
-<<<<<<< HEAD
 public class ProductMapFragment extends BaseDialogFragment
         implements ProductMapFragmentViewModel.Listener {
-=======
-public class ProductMapFragment extends BaseDialogFragment implements CallBackListener
-{
-    private static final String LOG_TAG = "ProductMap";
-    public static final String KEY_LOCATION = "key_location";
-    private CallBackListener callBackListener = this;
-    String FILENAME = "productMap.html";
->>>>>>> change productmap
 
     @Bind(R.id.location_webView)
     WebView webview;
@@ -64,60 +37,10 @@ public class ProductMapFragment extends BaseDialogFragment implements CallBackLi
         super.onActivityCreated(savedInstanceState);
 
         webview.getSettings().setJavaScriptEnabled(true);
-
         webview.getSettings().setBuiltInZoomControls(true);
-        webview.setWebChromeClient(new WebChromeClient());
-
-        //final String testUrl = "file:///android_asset/productMap.html?id=Werkbank";
-
-
-
-        //save
-        //new ConnectionTask().execute(productMapUrl);
-
-        //load
-        final String testUrl = "file:///" + getActivity().getFilesDir() + FILENAME;
-
-
-
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setBuiltInZoomControls(true);
-<<<<<<< HEAD
 
         mViewModel.setListener(this);
         mViewModel.initialize(getArguments());
-=======
-        webview.setWebChromeClient(new WebChromeClient());
-
-        try{
-        webview.loadUrl(testUrl);
-    } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        webview.setWebViewClient(new WebViewClient()
-        {
-
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error)
-            {
-
-                //Method 1
-                handler.proceed();
-
-                //Method 2
-                //ConnectionTask connectionTask = new ConnectionTask();
-                //connectionTask.setListener(callBackListener);
-
-                //connectionTask.execute(finalUrl);
-            }
-        });
-
-
-
-
->>>>>>> change productmap
     }
 
     @Override
@@ -141,77 +64,8 @@ public class ProductMapFragment extends BaseDialogFragment implements CallBackLi
     }
 
     @Override
-<<<<<<< HEAD
     public void onLoadMap(String url) {
         webview.loadUrl(url);
-=======
-    public void callback(String htmlText) {
-        try {
-            webview.loadData(htmlText, "text/html; charset=UTF-8", null);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // next release
-    private class ConnectionTask extends AsyncTask<String, Integer, String> {
-        private CallBackListener mListener;
-
-
-        public void setListener(CallBackListener listener) {
-            mListener = listener;
-        }
-
-
-        @Override
-        protected String doInBackground(String... urls) {
-            BufferedReader bufferedReader;
-            URL myUrl;
-            HttpsURLConnection urlConnection;
-            try {
-
-                myUrl = new URL(urls[0]);
-                urlConnection = (HttpsURLConnection) myUrl.openConnection();
-                urlConnection.setSSLSocketFactory(getPinnedCertSslSocketFactory());
-
-                bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-                String htmlText = "";
-                String line = "";
-                while ((line = bufferedReader.readLine()) != null)
-                    htmlText += line;
-
-
-
-
-                return htmlText;
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-
-
-        }
-
-        @Override
-        protected void onPostExecute(String htmlText)
-        {
-            try
-            {
-                FileOutputStream fos = getActivity().openFileOutput(FILENAME, Context.MODE_PRIVATE);
-                fos.write(htmlText.getBytes());
-                fos.close();
-            } catch (FileNotFoundException e)
-            {
-                e.printStackTrace();
-            } catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
->>>>>>> change productmap
     }
 
     @Override
--- app/src/main/java/de/fau/cs/mad/fablab/android/widget/WidgetProvider.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/widget/WidgetProvider.java
@@ -1,64 +1,134 @@
 package de.fau.cs.mad.fablab.android.widget;
 
+import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.widget.RemoteViews;
 
+import net.spaceapi.HackerSpace;
+import net.spaceapi.State;
+
 import java.lang.annotation.Annotation;
+import java.security.Provider;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.inject.Inject;
 
 import de.fau.cs.mad.fablab.android.R;
 import de.fau.cs.mad.fablab.android.model.SpaceApiModel;
+import de.fau.cs.mad.fablab.android.model.util.RestClient;
 import de.fau.cs.mad.fablab.android.util.Formatter;
 import de.fau.cs.mad.fablab.android.view.actionbar.ActionBarViewModel;
 import de.fau.cs.mad.fablab.android.view.activities.MainActivity;
-
-
+import de.fau.cs.mad.fablab.rest.myapi.SpaceApi;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 public class WidgetProvider extends AppWidgetProvider
 {
-//    @Inject
-//    ActionBarViewModel mViewModel;
-//
-//    @Inject
-//    SpaceApiModel mSpaceApiModel;
+    RestClient restClient;
+    SpaceApiModel mSpaceApiModel;
+    SpaceApi mSpaceApi;
 
-//    boolean isOpen;
-//    String widgetTime;
-//    final String SET_TIME = "set_time";
+    private boolean mOpen;
+    private long mTime;
+
+    public static final String FABLAB_WIDGET_UPDATE = "de.fau.cs.mad.fablab.android.widget.WidgetProvider.FABLAB_WIDGET_UPDATE";
+
+
+    private Callback<HackerSpace> mSpaceApiCallback = new Callback<HackerSpace>() {
+        @Override
+        public void success(HackerSpace hackerSpace, Response response) {
+            State state = hackerSpace.getState();
+            updateState(state.getOpen(), state.getLastchange());
+        }
+
+        @Override
+        public void failure(RetrofitError error) {
+
+        }
+    };
+
+    private void updateState(boolean open, double lastChange)
+    {
+        mOpen = open;
+
+        long currentTimeSeconds = System.currentTimeMillis() / 1000L;
+        double minutesSinceLastChange = (currentTimeSeconds - lastChange) / 60;
+        mTime = Double.valueOf(minutesSinceLastChange).longValue();
+    }
+
+
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
     {
+        final int N = appWidgetIds.length;
+        for(int i = 0; i<N; i++)
+        {
+            int appWidgetId = appWidgetIds[i];
+
+            // update a label
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            views.setTextViewText(R.id.widget_tv, "yeah");
+        }
+
+
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-
-        //isOpen = mSpaceApiModel.getOpen();
-        //widgetTime = Formatter.formatTime(mSpaceApiModel.getTime());
-
-//        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-//        ComponentName widgetComponent = new ComponentName(context, WidgetProvider.class);
-//
-//        remoteViews.setTextViewText(R.id.widget_tv, widgetTime);
-//        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-
-
+        Timer timer = new Timer();
+        new MyTime(context, appWidgetManager);
     }
 
-    @Override
-    public void onReceive(Context context, Intent intent)
+    private class MyTime extends TimerTask
     {
-        super.onReceive(context, intent);
+
+        RemoteViews remoteViews;
+        AppWidgetManager appWidgetManager;
+        ComponentName thisWidget;
+        SpaceApiModel spaceApiModel;
+        DateFormat format = SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM, Locale.getDefault());
+
+        public MyTime(Context context, AppWidgetManager appWidgetManager)
+        {
+            this.appWidgetManager = appWidgetManager;
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            thisWidget = new ComponentName(context, WidgetProvider.class);
+
+//            restClient = new RestClient(context, false);
+//            mSpaceApiModel = new SpaceApiModel(restClient.getSpaceApi(), context.getString(R.string.space_name), 6000);
+//            mTime = mSpaceApiModel.getTime();
+        }
+
+        @Override
+        public void run()
+        {
+
+            remoteViews.setTextViewText(R.id.widget_tv, "TIME = " + format.format(new Date())); //time test
+
+
+//            String time = Formatter.formatTime(mTime);
+//            remoteViews.setTextViewText(R.id.widget_tv, time);
+
+            appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+        }
 
 
     }
-
-
-
-
 }
