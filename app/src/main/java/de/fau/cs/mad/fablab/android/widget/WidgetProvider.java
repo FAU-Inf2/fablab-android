039edd5f084a63d72dba1bddd29f560d456a7030
michaelsandner <michaelsandy@gmx.de>
2015-10-08 18:34:22 +0200

    change widget layout and code. Widget doesn't get space status

--- app/src/main/java/de/fau/cs/mad/fablab/android/widget/WidgetProvider.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/widget/WidgetProvider.java
@@ -2,41 +2,25 @@ package de.fau.cs.mad.fablab.android.widget;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
-import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 import net.spaceapi.HackerSpace;
 import net.spaceapi.State;
 
-import java.lang.annotation.Annotation;
-import java.security.Provider;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import javax.inject.Inject;
 
 import de.fau.cs.mad.fablab.android.R;
-import de.fau.cs.mad.fablab.android.model.SpaceApiModel;
 import de.fau.cs.mad.fablab.android.model.util.RestClient;
 import de.fau.cs.mad.fablab.android.util.Formatter;
-import de.fau.cs.mad.fablab.android.view.actionbar.ActionBarViewModel;
-import de.fau.cs.mad.fablab.android.view.activities.MainActivity;
 import de.fau.cs.mad.fablab.rest.myapi.SpaceApi;
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -45,27 +29,35 @@ import retrofit.client.Response;
 
 public class WidgetProvider extends AppWidgetProvider
 {
-    RestClient restClient;
-    SpaceApiModel mSpaceApiModel;
-    SpaceApi mSpaceApi;
-    public static String DOOR_STATE_WIDGET_UPDATE = "de.fau.cs.mad.fablab.android.widget.DOOR_STATE_WIDGET_UPDATE";
-
+    private RestClient restClient;
+    private SpaceApi mSpaceApi;
+    private static String DOOR_STATE_WIDGET_UPDATE = "de.fau.cs.mad.fablab.android.widget.DOOR_STATE_WIDGET_UPDATE";
+    private static final int space_name = 0x7f0700af;
     private boolean mOpen;
-    private long mTime;
+    private double mTime;
+    private String mSpaceName;
+
+
+    private Handler mSpaceApiHandler = new Handler();
+    private Runnable mSpaceApiRunner = new SpaceApiRunner();
 
     private static final String LOG_TAG = "widget";
     public static final String FABLAB_WIDGET_UPDATE = "de.fau.cs.mad.fablab.android.widget.WidgetProvider.FABLAB_WIDGET_UPDATE";
+    private static final DateFormat df = new SimpleDateFormat("hh:mm:ss");
 
 
-    private Callback<HackerSpace> mSpaceApiCallback = new Callback<HackerSpace>() {
+    private Callback<HackerSpace> mSpaceApiCallback = new Callback<HackerSpace>()
+    {
         @Override
-        public void success(HackerSpace hackerSpace, Response response) {
+        public void success(HackerSpace hackerSpace, Response response)
+        {
             State state = hackerSpace.getState();
             updateState(state.getOpen(), state.getLastchange());
         }
 
         @Override
-        public void failure(RetrofitError error) {
+        public void failure(RetrofitError error)
+        {
 
         }
     };
@@ -82,21 +74,34 @@ public class WidgetProvider extends AppWidgetProvider
     {
         super.onEnabled(context);
         Log.d(LOG_TAG, "widget Provider enabled. Starting timer to update widget every second");
-        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
-        //Timer specific
+        mTime = 0.0;
+        mOpen = false;
+
+        restClient = new RestClient(context, false);
+        mSpaceApi = restClient.getSpaceApi();
+        mSpaceName = context.getString(R.string.space_name);
+
+        mSpaceApiHandler.removeCallbacks(mSpaceApiRunner);
+        mSpaceApi.getSpace(mSpaceName, mSpaceApiCallback);
+
         Calendar calender = Calendar.getInstance();
         calender.setTimeInMillis(System.currentTimeMillis());
         calender.add(Calendar.SECOND, 1);
-        alarmManager.setRepeating(AlarmManager.RTC, calender.getTimeInMillis(), 1000, createDoorStateIntent(context));
+
+        // update alarmManager each minute because the text view can display each minute
+        // creats an intent which calls onReceive each time interval
+        alarmManager.setRepeating(AlarmManager.RTC, calender.getTimeInMillis(), 10000, createDoorStateIntent(context));
     }
+
 
     @Override
     public void onDisabled(Context context)
     {
         super.onDisabled(context);
         Log.d(LOG_TAG, "Widget Provider disabled. Turning off timer");
-        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.cancel(createDoorStateIntent(context));
     }
 
@@ -105,79 +110,79 @@ public class WidgetProvider extends AppWidgetProvider
     {
         super.onReceive(context, intent);
         Log.d(LOG_TAG, "Received intent " + intent);
-        if(DOOR_STATE_WIDGET_UPDATE.equals(intent.getAction()))
+
+
+        if (DOOR_STATE_WIDGET_UPDATE.equals(intent.getAction()))
         {
             Log.d(LOG_TAG, "Door state update");
             // Get the widget manager and ids for this widget provider, then call the shared
             // door state update method
             ComponentName thisAppWidget = new ComponentName(context.getPackageName(), getClass().getName());
-
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidget);
+            for (int appWidgetID : ids)
+            {
+                updateAppWidget(context, appWidgetManager, appWidgetID, mTime, mOpen);
+            }
         }
     }
-
-    private void updateState(boolean open, double lastChange)
-    {
-        mOpen = open;
-
-        long currentTimeSeconds = System.currentTimeMillis() / 1000L;
-        double minutesSinceLastChange = (currentTimeSeconds - lastChange) / 60;
-        mTime = Double.valueOf(minutesSinceLastChange).longValue();
-    }
-
 
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
     {
-        final int N = appWidgetIds.length;
-        for(int i = 0; i<N; i++)
-        {
-            int appWidgetId = appWidgetIds[i];
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
 
-            // update a label
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            views.setTextViewText(R.id.widget_tv, "yeah");
+
+        for (int appWidgetID : appWidgetIds)
+        {
+            updateAppWidget(context, appWidgetManager, appWidgetID, mTime, mOpen);
         }
 
-
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-        Timer timer = new Timer();
-        new MyTime(context, appWidgetManager);
     }
 
-    private class MyTime extends TimerTask
+    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, double lastChange, boolean isOpen)
     {
 
-        RemoteViews remoteViews;
-        AppWidgetManager appWidgetManager;
-        ComponentName thisWidget;
-        SpaceApiModel spaceApiModel;
-        DateFormat format = SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM, Locale.getDefault());
+        long currentTimeSeconds = System.currentTimeMillis() / 1000L;
+        double minutesSinceLastChange = (currentTimeSeconds - lastChange) / 60;
+        long timeSinceLastChange = Double.valueOf(minutesSinceLastChange).longValue();
+        String timeSinceLastChangeAsString;
 
-        public MyTime(Context context, AppWidgetManager appWidgetManager)
+        if(lastChange == 0.0)
+            timeSinceLastChangeAsString = "0m";
+        else
+        timeSinceLastChangeAsString = Formatter.formatTime(timeSinceLastChange);
+
+        RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+        updateViews.setTextViewText(R.id.widget_tv, timeSinceLastChangeAsString);
+
+        if(isOpen)
         {
-            this.appWidgetManager = appWidgetManager;
-            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            thisWidget = new ComponentName(context, WidgetProvider.class);
-
-//            restClient = new RestClient(context, false);
-//            mSpaceApiModel = new SpaceApiModel(restClient.getSpaceApi(), context.getString(R.string.space_name), 6000);
-//            mTime = mSpaceApiModel.getTime();
+            updateViews.setTextColor(R.id.widget_tv, context.getResources().getColor(R.color.appbar_color_opened));
+            updateViews.setImageViewResource(R.id.widget_iv, R.drawable.opened);
         }
+        else
+        {
+            updateViews.setTextColor(R.id.widget_tv, context.getResources().getColor(R.color.appbar_color_closed));
+            updateViews.setImageViewResource(R.id.widget_iv, R.drawable.closed);
+        }
+        appWidgetManager.updateAppWidget(appWidgetId, updateViews);
+    }
+
+    public void updateState(boolean isOpen, double time)
+    {
+        mOpen = isOpen;
+        mTime = time;
+    }
+
+    private class SpaceApiRunner implements Runnable {
 
         @Override
-        public void run()
-        {
-
-            remoteViews.setTextViewText(R.id.widget_tv, "TIME = " + format.format(new Date())); //time test
-
-
-//            String time = Formatter.formatTime(mTime);
-//            remoteViews.setTextViewText(R.id.widget_tv, time);
-
-            appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+        public void run() {
+            mSpaceApi.getSpace(mSpaceName, mSpaceApiCallback);
         }
-
-
     }
+
+
 }
