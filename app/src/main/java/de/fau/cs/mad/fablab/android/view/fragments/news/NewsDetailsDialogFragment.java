7bf3ef8cf10cc28ef9667d2001d37375d02dec0c
Daniel Rosenmueller <info@roda-vision.de>
2015-10-10 15:12:53 +0200

    added animation in newsdialog to save screen space, added dialog to show image within app (not browser)

--- app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/news/NewsDetailsDialogFragment.java
+++ app/src/main/java/de/fau/cs/mad/fablab/android/view/fragments/news/NewsDetailsDialogFragment.java
@@ -1,14 +1,17 @@
 package de.fau.cs.mad.fablab.android.view.fragments.news;
 
-import android.content.Intent;
+import android.app.Dialog;
 import android.content.pm.ActivityInfo;
-import android.net.Uri;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -20,20 +23,25 @@ import de.fau.cs.mad.fablab.android.R;
 import de.fau.cs.mad.fablab.android.view.activities.MainActivity;
 import de.fau.cs.mad.fablab.android.view.common.binding.ViewCommandBinding;
 import de.fau.cs.mad.fablab.android.view.common.fragments.BaseDialogFragment;
+import de.fau.cs.mad.fablab.android.viewmodel.common.ObservableWebView;
 
 public class NewsDetailsDialogFragment extends BaseDialogFragment
         implements NewsDetailsDialogViewModel.Listener {
     @Bind(R.id.news_dialog_title)
     TextView title_tv;
     @Bind(R.id.news_dialog_webview)
-    WebView webView;
+    ObservableWebView webView;
     @Bind(R.id.news_dialog_image)
     ImageView image_iv;
     @Bind(R.id.news_dialog_link_tv)
     WebView link_tv;
 
+    LinearLayout title_ll;
+    LinearLayout header_ll;
+
     @Inject
     NewsDetailsDialogViewModel mViewModel;
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -44,10 +52,13 @@ public class NewsDetailsDialogFragment extends BaseDialogFragment
 
         new ViewCommandBinding().bind(image_iv, mViewModel.getImageClickCommand());
 
+        title_ll = (LinearLayout) getActivity().findViewById(R.id.news_dialog_title_ll);
+        header_ll = (LinearLayout) getActivity().findViewById(R.id.news_dialog_header_ll);
+
         title_tv.setText(mViewModel.getNews().getTitle());
 
         String stylesheet = "<link rel=\"stylesheet\" type=\"text/css\" href=\"news_dialog_stylesheet.css\" /> ";
-        String htmlData = stylesheet + mViewModel.getNews().getDescription();
+        String htmlData = stylesheet + "<div class=\"webview_content\">" + mViewModel.getNews().getDescription() + "</div>";
 
         webView.loadDataWithBaseURL("file:///android_asset/", htmlData, "text/html", "UTF-8", null);
         if (mViewModel.getNews().getLinkToPreviewImage() != null) {
@@ -55,6 +66,19 @@ public class NewsDetailsDialogFragment extends BaseDialogFragment
         } else {
             Picasso.with(image_iv.getContext()).load(R.drawable.news_nopicture).fit().into(image_iv);
         }
+
+        header_ll.bringToFront();
+
+        webView.setOnScrollChangedCallback(new ObservableWebView.OnScrollChangedCallback() {
+            @Override
+            public void onScroll(int l, int t) {
+                if(t < 180) {
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                    lp.setMargins(0, 0, 0, t);
+                    title_ll.setLayoutParams(lp);
+                }
+            }
+        });
 
         String link = stylesheet +
                 "<html><body class=\"link\"><a href=" +  mViewModel.getNews().getLink() + ">" + mViewModel.getNews().getLink() + "</a></body></html>";
@@ -76,8 +100,22 @@ public class NewsDetailsDialogFragment extends BaseDialogFragment
 
     @Override
     public void onImageClicked() {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(mViewModel.getNews().getLinkToPreviewImage()));
-        getActivity().startActivity(intent);
+        final Dialog builder = new Dialog(getActivity());
+        builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        builder.getWindow().setBackgroundDrawable(
+                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        ImageView imageView = new ImageView(getActivity());
+        Picasso.with(imageView.getContext()).load(mViewModel.getNews().getLinkToPreviewImage()).into(imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.dismiss();
+            }
+        });
+        builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        builder.show();
     }
 }
---
+++ app/src/main/java/de/fau/cs/mad/fablab/android/viewmodel/common/ObservableWebView.java
@@ -0,0 +1,42 @@
+package de.fau.cs.mad.fablab.android.viewmodel.common;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+public class ObservableWebView extends WebView
+{
+    private OnScrollChangedCallback mOnScrollChangedCallback;
+
+    public ObservableWebView(final Context context)
+    {
+        super(context);
+    }
+
+    public ObservableWebView(final Context context, final AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public ObservableWebView(final Context context, final AttributeSet attrs, final int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onScrollChanged(final int l, final int t, final int oldl, final int oldt)
+    {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if(mOnScrollChangedCallback != null) mOnScrollChangedCallback.onScroll(l, t);
+    }
+
+    public void setOnScrollChangedCallback(final OnScrollChangedCallback onScrollChangedCallback)
+    {
+        mOnScrollChangedCallback = onScrollChangedCallback;
+    }
+
+    public interface OnScrollChangedCallback
+    {
+        void onScroll(int l, int t);
+    }
+}
